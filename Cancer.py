534d149bf3beeb789e6f88bf22f189c3605e6afe
imtiazf1996 <136018965+imtiazf1996@users.noreply.github.com>
2023-10-07 22:05:32 -0400

    Cancer.py

--- Cancer.py
+++ Cancer.py
@@ -5,7 +5,12 @@ df1=pd.read_csv('data.csv')
 st.write("Fawaz Imtiaz")
 st.markdown('Welcome to my app')
 st.write("Breast Cancer Wisconsin Dataset")
-st.write(df1)
+show_table = st.checkbox("Show Dataset Table")
+df2 = df1.dropna(axis=1, how='any')
+
+
+if show_table:
+    st.write(df2)
 st.markdown("*")
 
 button=st.radio('Do you want to delete any row having NaN in at least one of the fields', ['No', 'Yes'])
@@ -21,6 +26,18 @@ if button1:
 if st.button("Hide Statistics"):
     button1=False
 
+
+selected_group = st.radio('Choose a feature group to keep:', ['Worst Features', 'Mean Features', 'Standard Error Features'])
+
+if selected_group == 'Worst Features':
+    df = df[['diagnosis'] + ['id'] + list(df.filter(like='worst'))]
+elif selected_group == 'Mean Features':
+    df = df[['diagnosis'] + ['id'] + list(df.filter(like='mean'))]
+elif selected_group == 'Standard Error Features':
+    df = df[['diagnosis'] + ['id'] + list(df.filter(like='se'))]
+else:
+    pass
+
 cols=df.columns
 red_df=df.iloc[:,0:32]
 red_cols=red_df.columns
@@ -31,6 +48,7 @@ if button2:
     st.write(df.columns)
 if st.button("Hide Columns"):
     button2=False
+
 st.write("Please select following variables for different plotting")
 xv=st.selectbox('Please select x or first variable:',cols)
 yv=st.selectbox('Please select y or second variiable:',cols)
