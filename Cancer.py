4ed9ba5e7ee63344763ae07b50b5a9a5c5ba6e87
imtiazf1996 <136018965+imtiazf1996@users.noreply.github.com>
2023-10-14 15:51:47 -0400

    Update Cancer.py

--- Cancer.py
+++ Cancer.py
@@ -56,15 +56,17 @@ if st.button("Hide Columns"):
 plot_selection = st.selectbox("Select a plot type:", ["Histogram", "Scatter Plot", "HiPlot", "Pair Plot", "Violin Plot"])
 
 st.write("Please select following variables for different plotting")
-if plot_selection in ["Histogram", "Scatter Plot", "Violin Plot", "Hiplot"]:
-    xv=st.selectbox('Please select x or first variable:',cols)
+if plot_selection in ["Histogram", "Scatter Plot", "Hiplot"]:
+    xv=st.selectbox('Please select x :',cols)
 
+if plot_selection in ["Violin Plot"]:
+    xv=df["diagnosis"]
 if plot_selection in ["Pair Plot"]:
     selected_box= st.multiselect('Select variables:', cols)
     selected_data = df[selected_box + ['diagnosis']]
 
 if plot_selection in [ "Scatter Plot", "HiPlot", "Violin Plot"]:
-    yv=st.selectbox('Please select y or second variiable:',cols)
+    yv=st.selectbox('Please select y :',cols)
 
 
 st.write("The hue in required plots will be based on Malignant (M) or Benign (B)")
