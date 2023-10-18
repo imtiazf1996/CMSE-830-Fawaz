02acad10c2280beec60462ff77d5a521bf84310a
imtiazf1996 <136018965+imtiazf1996@users.noreply.github.com>
2023-10-17 20:56:42 -0400

    Update Cancer.py

--- Cancer.py
+++ Cancer.py
@@ -152,15 +152,15 @@ if st.button("## What does the data tell us?"):
     df3 = df2[['diagnosis'] + ['id'] + list(df2.filter(like='mean'))]
     means = df3.groupby('diagnosis')[['radius_mean', 'texture_mean', 'perimeter_mean', 'area_mean','smoothness_mean','compactness_mean','concavity_mean','concave points_mean','symmetry_mean','fractal_dimension_mean']].mean()
     for col in means.columns:
-    if "mean" in col:
-        if means.loc['B', col] > means.loc['M', col]:
-            diff = means.loc['B', col] - means.loc['M', col]
-            perc = (diff / means.loc['M', col]) * 100
-            comparison[col] = f"Benign higher by {perc:.2f}%"
-        else:
-            diff = means.loc['M', col] - means.loc['B', col]
-            perc = (diff / means.loc['B', col]) * 100
-            comparison[col] = f"Malignant higher by {perc:.2f}%"
+        if "mean" in col:
+            if means.loc['B', col] > means.loc['M', col]:
+                diff = means.loc['B', col] - means.loc['M', col]
+                perc = (diff / means.loc['M', col]) * 100
+                comparison[col] = f"Benign higher by {perc:.2f}%"
+            else:
+                diff = means.loc['M', col] - means.loc['B', col]
+                perc = (diff / means.loc['B', col]) * 100
+                comparison[col] = f"Malignant higher by {perc:.2f}%"
     comp = pd.DataFrame(comparison, index=['Comparison'])
     means = pd.concat([means, comp])
     st.table(means)
