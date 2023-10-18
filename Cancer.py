f6510105895f438a32bde806e979cf0d59ca4820
imtiazf1996 <136018965+imtiazf1996@users.noreply.github.com>
2023-10-17 22:02:34 -0400

    Update Cancer.py

--- Cancer.py
+++ Cancer.py
@@ -93,9 +93,9 @@ if st.button("Generate Plot"):
         st.subheader("Histogram")
         st.markdown('[Know more about Histogram](https://en.wikipedia.org/wiki/Histogram)')
         b = st.slider("Number of bins", 5, 100, 20)
-        fig, ax = plt.subplots()
+        fig1, ax = plt.subplots()
         sns.histplot(data=df, x=xv, hue=zv,bins=b, kde=True)
-        st.pyplot(fig)
+        st.pyplot(fig1)
     
     elif plot_selection == "Scatter Plot":
         st.subheader("Scatter Plot")
