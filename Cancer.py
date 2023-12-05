59c973d2920ec142e30119f524251e9da403e1a6
imtiazf1996 <136018965+imtiazf1996@users.noreply.github.com>
2023-12-05 18:39:42 -0500

    Update Cancer.py

--- Cancer.py
+++ Cancer.py
@@ -207,7 +207,7 @@ if classifier_selection in ["Logistic Regression"]:
 
 elif classifier_selection in ["KNN"]:
    
-    knn = KNeighborsClassifier(n_neighbors=5)  
+    knn = KNeighborsClassifier(n_neighbors=2)  
     knn.fit(X_train, y_train)
     input_df = pd.DataFrame([input_data])
     input_df = scaler.transform(input_df)
