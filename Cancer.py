e09f08c0d5b98e7281d273ddb5af3ab03754718a
imtiazf1996 <136018965+imtiazf1996@users.noreply.github.com>
2023-12-05 17:28:41 -0500

    Update Cancer.py

--- Cancer.py
+++ Cancer.py
@@ -55,7 +55,7 @@ if section == 'General Info':
 ##Plots EDA
 
 elif section == 'Plots (EDA)':
-    selected_group = st.radio('Choose a feature group to keep:', ['Worst Features', 'Mean Features', 'Standard Error Features', 'Keep All'])
+        selected_group = st.radio('Choose a feature group to keep:', ['Worst Features', 'Mean Features', 'Standard Error Features', 'Keep All'])
         
         if selected_group == 'Worst Features':
             df = df[['diagnosis'] + ['id'] + list(df.filter(like='worst'))]
@@ -77,80 +77,80 @@ elif section == 'Plots (EDA)':
         if st.button("Hide Columns"):
             button2=False
 
-    plot_selection = st.selectbox("Select a plot type:", ["Histogram", "Scatter Plot", "Pair Plot", "Violin Plot", "3D Scatter Plot", "Correlation Heatmap", "HiPlot"])
-    
-    st.write("Please select following variables for different plotting")
-    if plot_selection in ["Histogram", "Scatter Plot", "3D Scatter Plot"]:
-        xv=st.selectbox('Please select x :',cols)
-    
-    if plot_selection in ["Pair Plot"]:
-        selected_box= st.multiselect('Select variables:', cols)
-        selected_data = df[selected_box + ['diagnosis']]
-    
-    if plot_selection in ["Correlation Heatmap"]:
-        selected_box= st.multiselect('Select variables:', cols)
-        selected_data = df[selected_box]
-    
-    if plot_selection in ["Violin Plot"]:
-        xv=df["diagnosis"]
-    
-    if plot_selection in [ "Scatter Plot", "Violin Plot", "3D Scatter Plot"]:
-        yv=st.selectbox('Please select y :',cols)
-    
-    if plot_selection in ["3D Scatter Plot"]:
-        z3=st.selectbox('Please select z:',cols)
-    
-    st.write("The hue in required plots will be based on Malignant (M) or Benign (B)")
-    zv='diagnosis'
-    
-    if st.button("Generate Plot"):
-        if plot_selection == "Histogram":
-            st.subheader("Histogram")
-            fig1, ax = plt.subplots()
-            sns.histplot(data=df, x=xv, hue=zv, kde=True)
-            st.pyplot(fig1)
+        plot_selection = st.selectbox("Select a plot type:", ["Histogram", "Scatter Plot", "Pair Plot", "Violin Plot", "3D Scatter Plot", "Correlation Heatmap", "HiPlot"])
         
-        elif plot_selection == "Scatter Plot":
-            st.subheader("Scatter Plot")
-            fig = px.scatter(df, x=xv, y=yv, color=zv, title="Scatter Plot")
-            fig.update_traces(marker=dict(size=6), selector=dict(mode='markers+text'))
-            fig.update_layout(hovermode='closest')
-            fig.update_traces(text=df[zv], textposition='top center')
-            st.plotly_chart(fig)
-    
-        elif plot_selection == "Pair Plot":
-            st.subheader("Pair Plot")
-            all_columns = selected_data.columns
-            exclude_column = 'diagnosis' 
-            dims = [col for col in all_columns if col != exclude_column]
-            fig = px.scatter_matrix(selected_data, dimensions = dims, title="Pair Plot", color= 'diagnosis')
-            fig.update_layout(plot_bgcolor="white")  
-            st.plotly_chart(fig)
-    
-        elif plot_selection == "3D Scatter Plot":
-            st.subheader("3D Scatter Plot")
-            fig = px.scatter_3d(df, x=xv, y=yv, z=z3, color='diagnosis')
-            st.plotly_chart(fig)
-    
-        elif plot_selection == "Correlation Heatmap":
-            st.subheader("Correlation Heatmap")
-            corr_matrix = selected_data.corr()
-            fig, ax = plt.subplots(figsize=(12, 10))
-            sns.heatmap(corr_matrix, annot=True, fmt=".2f", cmap='coolwarm', ax=ax)
-            plt.title('Correlation Heatmap')
-            st.pyplot(fig)
-    
-           
-        elif plot_selection == "Violin Plot":
-            st.subheader("Violin Plot")
-            fig = px.violin(df, x=xv, y=yv, color=zv, title="Violin Plot")
-            st.plotly_chart(fig)
-    
-        elif plot_selection == "HiPlot":
-            st.subheader("HiPlot")
-            hp = hip.Experiment.from_dataframe(df)
-            hiplot_html = hp.to_html()
-            st.components.v1.html(hp.to_html(), height = 800, width = 1600, scrolling=True)
+        st.write("Please select following variables for different plotting")
+        if plot_selection in ["Histogram", "Scatter Plot", "3D Scatter Plot"]:
+            xv=st.selectbox('Please select x :',cols)
+        
+        if plot_selection in ["Pair Plot"]:
+            selected_box= st.multiselect('Select variables:', cols)
+            selected_data = df[selected_box + ['diagnosis']]
+        
+        if plot_selection in ["Correlation Heatmap"]:
+            selected_box= st.multiselect('Select variables:', cols)
+            selected_data = df[selected_box]
+        
+        if plot_selection in ["Violin Plot"]:
+            xv=df["diagnosis"]
+        
+        if plot_selection in [ "Scatter Plot", "Violin Plot", "3D Scatter Plot"]:
+            yv=st.selectbox('Please select y :',cols)
+        
+        if plot_selection in ["3D Scatter Plot"]:
+            z3=st.selectbox('Please select z:',cols)
+        
+        st.write("The hue in required plots will be based on Malignant (M) or Benign (B)")
+        zv='diagnosis'
+        
+        if st.button("Generate Plot"):
+            if plot_selection == "Histogram":
+                st.subheader("Histogram")
+                fig1, ax = plt.subplots()
+                sns.histplot(data=df, x=xv, hue=zv, kde=True)
+                st.pyplot(fig1)
+            
+            elif plot_selection == "Scatter Plot":
+                st.subheader("Scatter Plot")
+                fig = px.scatter(df, x=xv, y=yv, color=zv, title="Scatter Plot")
+                fig.update_traces(marker=dict(size=6), selector=dict(mode='markers+text'))
+                fig.update_layout(hovermode='closest')
+                fig.update_traces(text=df[zv], textposition='top center')
+                st.plotly_chart(fig)
+        
+            elif plot_selection == "Pair Plot":
+                st.subheader("Pair Plot")
+                all_columns = selected_data.columns
+                exclude_column = 'diagnosis' 
+                dims = [col for col in all_columns if col != exclude_column]
+                fig = px.scatter_matrix(selected_data, dimensions = dims, title="Pair Plot", color= 'diagnosis')
+                fig.update_layout(plot_bgcolor="white")  
+                st.plotly_chart(fig)
+        
+            elif plot_selection == "3D Scatter Plot":
+                st.subheader("3D Scatter Plot")
+                fig = px.scatter_3d(df, x=xv, y=yv, z=z3, color='diagnosis')
+                st.plotly_chart(fig)
+        
+            elif plot_selection == "Correlation Heatmap":
+                st.subheader("Correlation Heatmap")
+                corr_matrix = selected_data.corr()
+                fig, ax = plt.subplots(figsize=(12, 10))
+                sns.heatmap(corr_matrix, annot=True, fmt=".2f", cmap='coolwarm', ax=ax)
+                plt.title('Correlation Heatmap')
+                st.pyplot(fig)
+        
+               
+            elif plot_selection == "Violin Plot":
+                st.subheader("Violin Plot")
+                fig = px.violin(df, x=xv, y=yv, color=zv, title="Violin Plot")
+                st.plotly_chart(fig)
+        
+            elif plot_selection == "HiPlot":
+                st.subheader("HiPlot")
+                hp = hip.Experiment.from_dataframe(df)
+                hiplot_html = hp.to_html()
+                st.components.v1.html(hp.to_html(), height = 800, width = 1600, scrolling=True)
 
 ##KNN
 
